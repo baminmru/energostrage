031548b36a1c418569ce12f8e52f27dc5d787560
baminmru <bami@nm.ru>
2020-01-16 17:31:12 +0300

    SYNC

---
+++ GrozaClient/frmWeekend.Designer.vb
@@ -0,0 +1,317 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmWeekend
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmWeekend))
+        Me.SplitContainer1 = New System.Windows.Forms.SplitContainer()
+        Me.tv = New Infragistics.Win.UltraWinTree.UltraTree()
+        Me.dv = New System.Windows.Forms.DataGridView()
+        Me.wb = New SpreadsheetGear.Windows.Forms.WorkbookView()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.cmdRefresh = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.dtpTo = New System.Windows.Forms.DateTimePicker()
+        Me.dtpFrom = New System.Windows.Forms.DateTimePicker()
+        Me.optPeriod = New System.Windows.Forms.RadioButton()
+        Me.opt30 = New System.Windows.Forms.RadioButton()
+        Me.opt14 = New System.Windows.Forms.RadioButton()
+        Me.opt7 = New System.Windows.Forms.RadioButton()
+        Me.opt1 = New System.Windows.Forms.RadioButton()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtHFrom = New System.Windows.Forms.NumericUpDown()
+        Me.txtHTo = New System.Windows.Forms.NumericUpDown()
+        Me.Label4 = New System.Windows.Forms.Label()
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SplitContainer1.Panel1.SuspendLayout()
+        Me.SplitContainer1.Panel2.SuspendLayout()
+        Me.SplitContainer1.SuspendLayout()
+        CType(Me.tv, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.dv, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel1.SuspendLayout()
+        CType(Me.txtHFrom, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.txtHTo, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'SplitContainer1
+        '
+        Me.SplitContainer1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.SplitContainer1.Location = New System.Drawing.Point(0, 0)
+        Me.SplitContainer1.Name = "SplitContainer1"
+        '
+        'SplitContainer1.Panel1
+        '
+        Me.SplitContainer1.Panel1.Controls.Add(Me.tv)
+        '
+        'SplitContainer1.Panel2
+        '
+        Me.SplitContainer1.Panel2.Controls.Add(Me.Panel1)
+        Me.SplitContainer1.Panel2.Controls.Add(Me.dv)
+        Me.SplitContainer1.Size = New System.Drawing.Size(949, 363)
+        Me.SplitContainer1.SplitterDistance = 315
+        Me.SplitContainer1.TabIndex = 0
+        '
+        'tv
+        '
+        Me.tv.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.tv.HideSelection = False
+        Me.tv.Location = New System.Drawing.Point(0, 0)
+        Me.tv.Name = "tv"
+        Me.tv.Size = New System.Drawing.Size(315, 363)
+        Me.tv.TabIndex = 0
+        '
+        'dv
+        '
+        Me.dv.AllowUserToAddRows = False
+        Me.dv.AllowUserToDeleteRows = False
+        Me.dv.AllowUserToOrderColumns = True
+        Me.dv.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.dv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dv.Location = New System.Drawing.Point(3, 77)
+        Me.dv.Name = "dv"
+        Me.dv.ReadOnly = True
+        Me.dv.Size = New System.Drawing.Size(624, 283)
+        Me.dv.TabIndex = 2
+        '
+        'wb
+        '
+        Me.wb.Location = New System.Drawing.Point(0, 0)
+        Me.wb.Name = "wb"
+        Me.wb.Size = New System.Drawing.Size(280, 160)
+        Me.wb.TabIndex = 0
+        Me.wb.Visible = False
+        Me.wb.WorkbookSetState = resources.GetString("wb.WorkbookSetState")
+        '
+        'SaveFileDialog1
+        '
+        Me.SaveFileDialog1.DefaultExt = "xls"
+        Me.SaveFileDialog1.Filter = "Excel file|*.xls"
+        '
+        'Panel1
+        '
+        Me.Panel1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Panel1.Controls.Add(Me.Label4)
+        Me.Panel1.Controls.Add(Me.txtHTo)
+        Me.Panel1.Controls.Add(Me.txtHFrom)
+        Me.Panel1.Controls.Add(Me.Label3)
+        Me.Panel1.Controls.Add(Me.cmdRefresh)
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Controls.Add(Me.dtpTo)
+        Me.Panel1.Controls.Add(Me.dtpFrom)
+        Me.Panel1.Controls.Add(Me.optPeriod)
+        Me.Panel1.Controls.Add(Me.opt30)
+        Me.Panel1.Controls.Add(Me.opt14)
+        Me.Panel1.Controls.Add(Me.opt7)
+        Me.Panel1.Controls.Add(Me.opt1)
+        Me.Panel1.Location = New System.Drawing.Point(4, 3)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(626, 68)
+        Me.Panel1.TabIndex = 3
+        '
+        'cmdRefresh
+        '
+        Me.cmdRefresh.Location = New System.Drawing.Point(390, 4)
+        Me.cmdRefresh.Name = "cmdRefresh"
+        Me.cmdRefresh.Size = New System.Drawing.Size(179, 22)
+        Me.cmdRefresh.TabIndex = 10
+        Me.cmdRefresh.Text = "Обновить"
+        Me.cmdRefresh.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(155, 39)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(21, 13)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "По"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(11, 39)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(14, 13)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "С"
+        '
+        'dtpTo
+        '
+        Me.dtpTo.Enabled = False
+        Me.dtpTo.Location = New System.Drawing.Point(182, 33)
+        Me.dtpTo.Name = "dtpTo"
+        Me.dtpTo.Size = New System.Drawing.Size(137, 20)
+        Me.dtpTo.TabIndex = 6
+        '
+        'dtpFrom
+        '
+        Me.dtpFrom.Enabled = False
+        Me.dtpFrom.Location = New System.Drawing.Point(31, 33)
+        Me.dtpFrom.Name = "dtpFrom"
+        Me.dtpFrom.Size = New System.Drawing.Size(118, 20)
+        Me.dtpFrom.TabIndex = 5
+        '
+        'optPeriod
+        '
+        Me.optPeriod.AutoSize = True
+        Me.optPeriod.Location = New System.Drawing.Point(180, 8)
+        Me.optPeriod.Name = "optPeriod"
+        Me.optPeriod.Size = New System.Drawing.Size(51, 17)
+        Me.optPeriod.TabIndex = 4
+        Me.optPeriod.Tag = "0"
+        Me.optPeriod.Text = "С-ПО"
+        Me.optPeriod.UseVisualStyleBackColor = True
+        '
+        'opt30
+        '
+        Me.opt30.AutoSize = True
+        Me.opt30.Location = New System.Drawing.Point(128, 9)
+        Me.opt30.Name = "opt30"
+        Me.opt30.Size = New System.Drawing.Size(43, 17)
+        Me.opt30.TabIndex = 3
+        Me.opt30.Tag = "180"
+        Me.opt30.Text = "180"
+        Me.opt30.UseVisualStyleBackColor = True
+        '
+        'opt14
+        '
+        Me.opt14.AutoSize = True
+        Me.opt14.Checked = True
+        Me.opt14.Location = New System.Drawing.Point(85, 9)
+        Me.opt14.Name = "opt14"
+        Me.opt14.Size = New System.Drawing.Size(37, 17)
+        Me.opt14.TabIndex = 2
+        Me.opt14.TabStop = True
+        Me.opt14.Tag = "90"
+        Me.opt14.Text = "90"
+        Me.opt14.UseVisualStyleBackColor = True
+        '
+        'opt7
+        '
+        Me.opt7.AutoSize = True
+        Me.opt7.Location = New System.Drawing.Point(48, 10)
+        Me.opt7.Name = "opt7"
+        Me.opt7.Size = New System.Drawing.Size(37, 17)
+        Me.opt7.TabIndex = 1
+        Me.opt7.Tag = "30"
+        Me.opt7.Text = "30"
+        Me.opt7.UseVisualStyleBackColor = True
+        '
+        'opt1
+        '
+        Me.opt1.AutoSize = True
+        Me.opt1.Location = New System.Drawing.Point(11, 10)
+        Me.opt1.Name = "opt1"
+        Me.opt1.Size = New System.Drawing.Size(37, 17)
+        Me.opt1.TabIndex = 0
+        Me.opt1.Tag = "10"
+        Me.opt1.Text = "10"
+        Me.opt1.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(353, 39)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(80, 13)
+        Me.Label3.TabIndex = 11
+        Me.Label3.Text = "Рабочие часы:"
+        '
+        'txtHFrom
+        '
+        Me.txtHFrom.Location = New System.Drawing.Point(439, 33)
+        Me.txtHFrom.Maximum = New Decimal(New Integer() {12, 0, 0, 0})
+        Me.txtHFrom.Name = "txtHFrom"
+        Me.txtHFrom.Size = New System.Drawing.Size(47, 20)
+        Me.txtHFrom.TabIndex = 12
+        Me.txtHFrom.Value = New Decimal(New Integer() {8, 0, 0, 0})
+        '
+        'txtHTo
+        '
+        Me.txtHTo.Location = New System.Drawing.Point(522, 32)
+        Me.txtHTo.Maximum = New Decimal(New Integer() {23, 0, 0, 0})
+        Me.txtHTo.Minimum = New Decimal(New Integer() {13, 0, 0, 0})
+        Me.txtHTo.Name = "txtHTo"
+        Me.txtHTo.Size = New System.Drawing.Size(47, 20)
+        Me.txtHTo.TabIndex = 13
+        Me.txtHTo.Value = New Decimal(New Integer() {20, 0, 0, 0})
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(492, 35)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(10, 13)
+        Me.Label4.TabIndex = 14
+        Me.Label4.Text = "-"
+        '
+        'frmWeekend
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(949, 363)
+        Me.Controls.Add(Me.SplitContainer1)
+        Me.Controls.Add(Me.wb)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "frmWeekend"
+        Me.Text = "Статистические показатели"
+        Me.SplitContainer1.Panel1.ResumeLayout(False)
+        Me.SplitContainer1.Panel2.ResumeLayout(False)
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.SplitContainer1.ResumeLayout(False)
+        CType(Me.tv, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.dv, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        CType(Me.txtHFrom, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.txtHTo, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents SplitContainer1 As System.Windows.Forms.SplitContainer
+    Friend WithEvents tv As Infragistics.Win.UltraWinTree.UltraTree
+    Friend WithEvents wb As SpreadsheetGear.Windows.Forms.WorkbookView
+    Friend WithEvents SaveFileDialog1 As System.Windows.Forms.SaveFileDialog
+    Friend WithEvents dv As System.Windows.Forms.DataGridView
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents cmdRefresh As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents dtpTo As DateTimePicker
+    Friend WithEvents dtpFrom As DateTimePicker
+    Friend WithEvents optPeriod As RadioButton
+    Friend WithEvents opt30 As RadioButton
+    Friend WithEvents opt14 As RadioButton
+    Friend WithEvents opt7 As RadioButton
+    Friend WithEvents opt1 As RadioButton
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtHTo As NumericUpDown
+    Friend WithEvents txtHFrom As NumericUpDown
+    Friend WithEvents Label3 As Label
+End Class
---
+++ GrozaClient/frmWeekend.vb
@@ -0,0 +1,230 @@
+﻿Imports Infragistics.Win
+Imports Infragistics.Win.UltraWinTree
+'Imports SpreadsheetGear
+Imports Infragistics.UltraChart.Shared.Styles
+
+Public Class frmWeekend
+
+    'Private WithEvents workbook As IWorkbook
+    'Private WithEvents outworkbook As IWorkbook
+    'Private WithEvents ws As IWorksheet
+    'Private WithEvents outws As IWorksheet
+
+    Private Sub frmWeekend_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
+
+        LoadTree(tv)
+        Me.WindowState = FormWindowState.Maximized
+    End Sub
+
+    'Private Sub LoadTree()
+    '    tv.Nodes.Clear()
+    '    Dim dt As DataTable
+    '    dt = tvmain.QuerySelect("select * from esender")
+    '    Dim n As UltraTreeNode
+
+    '    Dim i As Integer
+    '    For i = 0 To dt.Rows.Count - 1
+    '        n = New UltraTreeNode("esender:" + dt.Rows(i)("sender_id").ToString, dt.Rows(i)("sender_name") + " (" + dt.Rows(i)("sender_inn") + ")")
+    '        tv.Nodes.Add(n)
+    '        n.Tag = dt.Rows(i)("sender_id")
+    '        LoadNodes(n, dt.Rows(i)("sender_id"))
+    '    Next
+
+    'End Sub
+
+    'Private Sub LoadNodes(ByVal p As UltraTreeNode, ByVal sender_id As Integer)
+    '    Dim dt As DataTable
+    '    dt = tvmain.QuerySelect("select * from enodes where sender_id=" + sender_id.ToString() + " order by mpoint_name")
+    '    Dim n As UltraTreeNode
+    '    Dim i As Integer
+    '    For i = 0 To dt.Rows.Count - 1
+    '        Try
+    '            n = New UltraTreeNode("enodes:" + dt.Rows(i)("node_id").ToString, dt.Rows(i)("mpoint_name") + " (" + dt.Rows(i)("mpoint_code") + ")")
+    '            n.Tag = dt.Rows(i)("node_id")
+    '            p.Nodes.Add(n)
+    '        Catch ex As Exception
+    '            MsgBox(ex.Message)
+    '        End Try
+
+
+    '    Next
+    'End Sub
+
+    Private Sub tv_AfterSelect(ByVal sender As Object, ByVal e As SelectEventArgs) Handles tv.AfterSelect
+        Dim n As UltraTreeNode = Nothing
+
+        If tv.SelectedNodes.Count > 0 Then
+            n = tv.SelectedNodes.Item(0)
+        End If
+        If n Is Nothing Then Exit Sub
+        Dim id As Integer
+        If n.Key.ToString().StartsWith("esender:") Then
+            If n.HasNodes() = False Then
+                LoadNodes(n, n.Tag)
+            End If
+
+        End If
+        If n.Key.ToString().StartsWith("enodes:") Then
+            id = n.Tag
+            Dim dt As DataTable
+
+
+            Dim w As String = "where 1=1 "
+
+            If opt1.Checked Then
+                w = " where p_date >= sysdate -10 "
+            End If
+
+            If opt7.Checked Then
+                w = " where p_date >= sysdate - 30 "
+            End If
+
+            If opt14.Checked Then
+                w = " where p_date >= sysdate - 90 "
+            End If
+
+            If opt30.Checked Then
+                w = " where p_date >= sysdate - 180 "
+            End If
+
+            If optPeriod.Checked Then
+                w = " where p_date >= " + tvmain.OracleDate(dtpFrom.Value) + " and p_date <=" + tvmain.OracleDate(dtpTo.Value)
+            End If
+
+
+
+            Dim q As String = ""
+            q = "SELECT CAST(avg(AP) as number(18,6)) AP, CAST(avg(AM) as number(18,6)) AM, CAST(avg(RP) as number(18,6)) RP, CAST(avg(RM) as number(18,6)) RM ,cast(p_HOUR AS NUMBER(2)) P_HOUR , DW  
+            FROM EDATA_HOUR d
+            " & w & " and d.node_id=" & id.ToString & "  GROUP BY p_hour,dw "
+
+
+
+            Dim ap(24) As Double
+            Dim wap(24) As Double
+            Dim am(24) As Double
+            Dim wam(24) As Double
+            Dim rp(24) As Double
+            Dim wrp(24) As Double
+            Dim rm(24) As Double
+            Dim wrm(24) As Double
+
+
+
+
+
+
+            dt = tvmain.QuerySelect(q)
+            For i = 0 To dt.Rows.Count - 1
+                If dt.Rows(i)("DW") = "SAT" Or dt.Rows(i)("DW") = "SUN" Or dt.Rows(i)("DW") = "ВС" Or dt.Rows(i)("DW") = "СБ" Then
+                    wap(dt.Rows(i)("P_HOUR")) += dt.Rows(i)("AP")
+                    wam(dt.Rows(i)("P_HOUR")) += dt.Rows(i)("AM")
+                    wrp(dt.Rows(i)("P_HOUR")) += dt.Rows(i)("RP")
+                    wrm(dt.Rows(i)("P_HOUR")) += dt.Rows(i)("RM")
+                Else
+                    ap(dt.Rows(i)("P_HOUR")) += dt.Rows(i)("AP")
+                    am(dt.Rows(i)("P_HOUR")) += dt.Rows(i)("AM")
+                    rp(dt.Rows(i)("P_HOUR")) += dt.Rows(i)("RP")
+                    rm(dt.Rows(i)("P_HOUR")) += dt.Rows(i)("RM")
+                End If
+            Next
+
+            Dim dt2 As DataTable
+            Dim dr As DataRow
+            dt2 = New DataTable
+            dt2.Columns.Add("Тип дня")
+            dt2.Columns.Add("Суммарно за день")
+            dt2.Columns.Add("Час 0")
+            dt2.Columns.Add("Час 1")
+            dt2.Columns.Add("Час 2")
+            dt2.Columns.Add("Час 3")
+            dt2.Columns.Add("Час 4")
+            dt2.Columns.Add("Час 5")
+            dt2.Columns.Add("Час 6")
+            dt2.Columns.Add("Час 7")
+            dt2.Columns.Add("Час 8")
+            dt2.Columns.Add("Час 9")
+            dt2.Columns.Add("Час 10")
+            dt2.Columns.Add("Час 11")
+            dt2.Columns.Add("Час 12")
+            dt2.Columns.Add("Час 13")
+            dt2.Columns.Add("Час 14")
+            dt2.Columns.Add("Час 15")
+            dt2.Columns.Add("Час 16")
+            dt2.Columns.Add("Час 17")
+            dt2.Columns.Add("Час 18")
+            dt2.Columns.Add("Час 19")
+            dt2.Columns.Add("Час 20")
+            dt2.Columns.Add("Час 21")
+            dt2.Columns.Add("Час 22")
+            dt2.Columns.Add("Час 23")
+
+            dr = dt2.NewRow
+            dr("Тип дня") = "Рабочие"
+            Dim ss As Double
+            ss = 0
+            For i = 0 To 23
+                dr("Час " + i.ToString()) = ap(i) + rp(i)
+                ss += (ap(i) + rp(i))
+            Next
+            dr("Суммарно за день") = ss
+
+            dt2.Rows.Add(dr)
+
+            dr = dt2.NewRow
+            dr("Тип дня") = "Выходные"
+            ss = 0
+            For i = 0 To 23
+                dr("Час " + i.ToString()) = wap(i) + wrp(i)
+                ss += (wap(i) + wrp(i))
+            Next
+            dr("Суммарно за день") = ss
+
+            dt2.Rows.Add(dr)
+
+
+
+            dr = dt2.NewRow
+            dr("Тип дня") = "Разница"
+            ss = 0
+            For i = 0 To 23
+                dr("Час " + i.ToString()) = (ap(i) + rp(i)) - (wap(i) + wrp(i))
+                ss += (ap(i) + rp(i)) - (wap(i) + wrp(i))
+            Next
+            dr("Суммарно за день") = ss
+
+            dt2.Rows.Add(dr)
+
+
+            dr = dt2.NewRow
+            dr("Тип дня") = "Разница в рабочие часы"
+            ss = 0
+            For i = txtHFrom.Value To txtHTo.Value
+
+                dr("Час " + i.ToString()) = (ap(i) + rp(i)) - (wap(i) + wrp(i))
+                ss += (ap(i) + rp(i)) - (wap(i) + wrp(i))
+            Next
+            dr("Суммарно за день") = ss
+
+            dt2.Rows.Add(dr)
+
+            dv.DataSource = dt2
+
+
+        End If
+    End Sub
+
+
+
+
+
+
+
+    Private Sub CHART_A_ChartDataClicked(ByVal sender As System.Object, ByVal e As Infragistics.UltraChart.Shared.Events.ChartDataEventArgs)
+
+    End Sub
+
+    Private Sub cmdRefresh_Click(sender As Object, e As EventArgs) Handles cmdRefresh.Click
+        tv_AfterSelect(sender, nothing)
+    End Sub
+End Class
